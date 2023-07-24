8e86347800b055514209cfd855f69732226bc3f8
Francesco Landolfi <flandolfi@users.noreply.github.com>
2023-07-24 15:31:28 +0200

    Add optional parameter and dynamic variant

--- gconv/conv.py
+++ gconv/conv.py
@@ -1,11 +1,11 @@
 import math
-from typing import Optional
+from typing import Optional, Union
 
 import torch
 from torch import Tensor
-from torch.nn import Parameter, Module
+from torch.nn import Parameter, Module, functional as F
 
-from torch_geometric.nn import inits
+from torch_geometric.nn import inits, knn_graph
 from torch_geometric.utils import scatter
 from torch_geometric.typing import OptTensor, Adj
 
@@ -15,10 +15,12 @@ class GenConv(Module):
                  out_channels: Optional[int] = None,
                  pos_channels: Optional[int] = None,
                  bias: bool = True,
-                 num_offsets: int = 16,
+                 num_offsets: int = 8,
                  trainable_offsets: bool = True,
                  metric: str = 'euclidean',
+                 temperature: Union[float, str] = 'same',
                  depthwise: bool = False,
+                 aggr: str = 'add',
                  offset_initializer: str = 'uniform',
                  weight_initializer: str = 'kaiming_uniform',
                  bias_initializer: str = 'uniform'):
@@ -31,6 +33,11 @@ class GenConv(Module):
         self.num_offsets = num_offsets
         self.metric = metric
         self.depthwise = depthwise
+        self.aggr = aggr
+        self.temperature = temperature
+
+        if temperature == 'same':
+            self.temperature = self.num_offsets
 
         self.offset_initializer = offset_initializer
         self.weight_initializer = weight_initializer
@@ -42,10 +49,10 @@ class GenConv(Module):
         if self.depthwise:
             self.weight = Parameter(Tensor(self.num_offsets, self.in_channels))
         else:
-            self.weight = Parameter(Tensor(self.num_offsets, self.out_channels, self.in_channels))
+            self.weight = Parameter(Tensor(self.num_offsets, self.out_channels*self.in_channels))
 
         if bias:
-            self.bias = Parameter(Tensor(self.num_offsets, self.out_channels))
+            self.bias = Parameter(Tensor(1, self.out_channels))
         else:
             self.register_parameter('bias', None)
 
@@ -71,6 +78,18 @@ class GenConv(Module):
     def pairwise_similarity(self, offset: Tensor) -> Tensor:
         if self.metric == 'euclidean':
             return -torch.cdist(offset, self.offset)
+        
+        if self.metric == 'cosine':
+            obs = F.normalize(offset, p=2, dim=-1)
+            exp = F.normalize(self.offset, p=2, dim=-1)
+
+            return obs @ exp.T
+        
+        if self.metric == 'delta-norm':
+            obs = torch.norm(offset, p=2, dim=-1, keepdim=True)
+            exp = torch.norm(self.offset, p=2, dim=-1, keepdim=True)
+
+            return -torch.abs(obs - exp.T)
 
         return offset @ self.offset.T
 
@@ -89,17 +108,42 @@ class GenConv(Module):
             val = torch.ones_like(row, dtype=torch.float)
 
         sim = self.pairwise_similarity(pos[col] - pos[row])
-        alpha = torch.softmax(sim, dim=-1)
+        alpha = torch.softmax(sim*self.temperature, dim=-1)
 
-        W = torch.einsum('ij,j...->i...', alpha, self.weight)
+        W = alpha @ self.weight
 
         if self.depthwise:
             msg = W*x[col]
         else:
-            msg = torch.bmm(W, x[col].unsqueeze(-1)).squeeze(-1)
+            msg = torch.bmm(W.view(-1, self.out_channels, self.in_channels), 
+                            x[col].unsqueeze(-1)).squeeze(-1)
+        
+        out = scatter(msg*val.view(-1, 1), row, dim=0, 
+                      dim_size=x.size(0), reduce=self.aggr)
 
         if self.bias is not None:
-            b = alpha @ self.bias
-            msg = msg + b
+            out = out + self.bias
 
-        return scatter(msg*val.view(-1, 1), row, dim=0, dim_size=x.size(0))
+        return out
+
+
+class DynamicGenConv(GenConv):
+    def __init__(self, in_channels: int,
+                 out_channels: Optional[int] = None,
+                 pos_channels: Optional[int] = None, 
+                 k: int = 20,
+                 loop: bool = True,
+                 *args, **kwargs):
+        super().__init__(in_channels=in_channels,
+                         out_channels=out_channels,
+                         pos_channels=pos_channels,
+                         *args, **kwargs)
+        self.k = k
+        self.loop = loop
+
+    def forward(self, x: Tensor, pos: OptTensor = None, batch: OptTensor = None) -> Tensor:
+        if pos is None:
+            pos = x
+        
+        edge_index = knn_graph(pos, k=self.k, batch=batch, loop=self.loop)
+        return super().forward(x, edge_index, None, pos)
