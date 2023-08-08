f38428351abe74a64d6e0188945138edce332c57
Francesco Landolfi <flandolfi@users.noreply.github.com>
2023-08-08 17:39:59 +0200

    Use graph transform for point clouds

--- test/test_point_conv.py
+++ test/test_point_conv.py
@@ -4,12 +4,12 @@ import torch
 from torch.nn import Conv1d, Conv2d
 
 from gconv.conv import GenPointConv
-from benchmark.transforms import ImageToPointCloud, SequenceToPointCloud
+from benchmark.transforms import ImageToGraph, SequenceToGraph
 
 
 @pytest.mark.parametrize('in_channels', [4, 8, 16])
 @pytest.mark.parametrize('out_channels', [4, 8, 16])
-@pytest.mark.parametrize('groups', [2, 4])
+@pytest.mark.parametrize('groups', [1, 2, 4])
 @pytest.mark.parametrize('length', [3, 6, 9, 12, 15])
 @pytest.mark.parametrize('seed', [0, 1, 2])
 def test_conv_1d(in_channels, out_channels, groups, length, seed):
@@ -22,7 +22,7 @@ def test_conv_1d(in_channels, out_channels, groups, length, seed):
     exp_out = conv(rand_sequence)
 
     graph_conv = GenPointConv.from_regular_conv(conv, temperature='inf')
-    data = SequenceToPointCloud()(rand_sequence)
+    data = SequenceToGraph()(rand_sequence)
     obs_out = graph_conv(x=data.x, pos=data.pos)
 
     assert torch.allclose(exp_out[:, 1:-1], obs_out.T[:, 1:-1], rtol=1e-4, atol=1e-7)
@@ -36,7 +36,7 @@ def test_conv_1d(in_channels, out_channels, groups, length, seed):
 @pytest.mark.parametrize('seed', [0, 1, 2])
 def test_conv_2d(in_channels, out_channels, groups, width, height, seed):
     torch.random.manual_seed(seed)
-    transform = ImageToPointCloud()
+    transform = ImageToGraph()
     rand_image = torch.rand(in_channels, width, height)
 
     conv = Conv2d(in_channels, out_channels,
