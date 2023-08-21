2e591351416f7d5c58f33817e944e647052c3580
Francesco Landolfi <flandolfi@users.noreply.github.com>
2023-08-21 18:47:53 +0200

    Make BaseGenConv accessible

--- gconv/conv/__init__.py
+++ gconv/conv/__init__.py
@@ -1,7 +1,9 @@
 from .graph_conv import GenGraphConv
 from .point_conv import GenPointConv
+from .base_conv import BaseGenConv
 
 __all__ = [
+    'BaseGenConv',
     'GenGraphConv',
     'GenPointConv',
 ]
